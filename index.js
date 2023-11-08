1a35b748587c09146d168c10ad58a3879c7728e7
sabir689 <sahariarpub@gmail.com>
2023-11-08 12:58:25 +0600

    patch in mongo from bookings

--- index.js
+++ index.js
@@ -107,7 +107,19 @@ async function run() {
       const result = await bookingCollection.insertOne(booking)
       res.send(result);
     })
-
+    app.patch('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedBooking = req.body;
+      console.log(updatedBooking);
+      const updateDoc = {
+        $set: {
+          status: updatedBooking.status
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
 
  
 
